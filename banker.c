8e5857ae76ac06b44e2326fc16425723a39868a4
Francisco Antonio da Luz Filho <falf@cesar.school>
2023-11-22 16:00:25 -0300

    Update banker.c

--- banker.c
+++ banker.c
@@ -6,28 +6,26 @@
 
 
 
-typedef struct Node {
-    int value;
-    struct Node *next;
-} Node;
 
 
-Node* append(Node *head, int value);
-void freeList(Node *head);
+
+
+
 int count_numbers_in_line(char *line);
 int count_lines(FILE *file);
 int count_whitespace(FILE *file);
 void check_command_type(const char *line);
 void allocate_resources(int customer, int request[]);
 void release_resources(int customer, int release[]);
-int is_safe_state(int customer, int request[]);
+void get_numbers(FILE *file, int count, int numbers[][count]); 
+int is_safe_state(int customer, int request[], int count_resources, int available[], int customer_resources[][count_resources]);
 
 
 
 int main(int argc, char *argv[]) {
     char line[1024];
     int count_resources = 0;
-
+    int resources[argc-1];
 
     FILE *customer_file = fopen("customer.txt", "r");
     if (customer_file == NULL) {
@@ -54,10 +52,8 @@ int main(int argc, char *argv[]) {
 
     }
 
-    Node *head = NULL;
-    for (int i = 1; i < argc; i++) {
-        int value = atoi(argv[i]);
-        head = append(head, value);
+    for (int i = 0; i < argc - 1; i++) {
+        resources[i] = atoi(argv[i+1]);
     }
     rewind(customer_file);
     int customer = count_lines(customer_file);
@@ -85,6 +81,12 @@ int main(int argc, char *argv[]) {
         current_customer++;
     }
 
+
+    for (int i = 0; i < count_resources; i++) {
+        printf("Resource %d: %d\n", i, resources[i]);
+    }
+
+
     for (int i = 0; i < customer; i++) {
         printf("Customer %d resources:", i);
         for (int j = 0; j < count_resources; j++) {
@@ -93,26 +95,41 @@ int main(int argc, char *argv[]) {
         printf("\n");
     }
     rewind(commands_file);
-    int request[count_resources];
+    int request[customer][count_resources];
+
+    get_numbers(commands_file, count_resources, request);
+
+    for(int i = 0; i < customer; i++){
+        for(int j = 0; j < count_resources; j++){
+            printf("%d ", request[i][j]);
+        }
+        printf("\n");
+    }
 
     while(fgets(line, 1024, commands_file) != NULL) {
         if (strstr(line, "RQ") != NULL) {
             int flag = 0;
             int customer_number;
             sscanf(line, "RQ %d", &customer_number);
-            Node *current = head;
-            for(int i = 0; i < count_resources; i++){
-                sscanf(line, "%d", &request[i]);
-                if(request[i] > customer_resources[customer_number][i]){
+
+
+            // Criar uma cópia da linha original
+            char line_copy[1024];
+            strcpy(line_copy, line);
+
+            for (int i = 0; i < count_resources; i++) {
+                printf("%d\n", request[customer_number][i]);
+                if (request[customer_number][i] > customer_resources[customer_number][i]) {
                     flag = 1;
-                }
-                else if(request[i] > current->value){
+                } else if (request[customer_number][i] > resources[i]) {
                     flag = 2;
-                }else if(i == count_resources - 1 && is_safe_state(customer_number, request) == 1){
-                    flag = 3;
                 }
-                current = current->next;
             }
+
+            if(is_safe_state(customer_number, request, count_resources, resources, customer_resources) == 0){
+                flag = 3;
+            }
+
             switch(flag){
                 case 1:
                     printf("The customer %d request was denied because exceed its maximum need\n", customer_number);
@@ -123,18 +140,21 @@ int main(int argc, char *argv[]) {
                 case 3:
                     printf("The customer %d request was denied because result in an unsafe state\n", customer_number);
                     break;
+                default:
+                    printf("The customer %d request was accepted\n", customer_number);
+                    for(int i = 0; i < count_resources; i++){
+                        resources[i] -= request[customer_number][i];
+                        customer_resources[customer_number][i] += request[customer_number][i];
+                    }
             }
-            
-            
-
         } else if (strstr(line, "RL") != NULL) {
             int customer_number;
-            sscanf(line, "RQ %d", &customer_number);
+            sscanf(line, "RL %d", &customer_number); // Corrigir aqui
             for(int i = 0; i < count_resources; i++){
-                sscanf(line, "%d", &request[i]);
+                sscanf(line, "%d", &request[customer_number][i]);
+                resources[i] += request[customer_number][i]; // Atualizar recursos disponíveis
+                customer_resources[customer_number][i] -= request[customer_number][i]; // Atualizar recursos alocados para o cliente
             }
-            
-        
         } else if (strstr(line, "*") != NULL) {
        
         
@@ -146,7 +166,6 @@ int main(int argc, char *argv[]) {
 
 
 
-    freeList(head);
 
     fclose(customer_file);
     fclose(commands_file);
@@ -156,39 +175,7 @@ int main(int argc, char *argv[]) {
 
 
 
-Node* append(Node *head, int value) {
-    Node *newNode = (Node*)malloc(sizeof(Node));
-    if (newNode == NULL) {
-        fprintf(stderr, "Memory allocation failed.\n");
-        exit(EXIT_FAILURE);
-    }
 
-    newNode->value = value;
-    newNode->next = NULL;
-
-    if (head == NULL) {
-        return newNode;
-    }
-
-    Node *current = head;
-    while (current->next != NULL) {
-        current = current->next;
-    }
-
-    current->next = newNode;
-    return head;
-}
-
-void freeList(Node *head) {
-    Node *current = head;
-    Node *next;
-
-    while (current != NULL) {
-        next = current->next;
-        free(current);
-        current = next;
-    }
-}
 
 int count_numbers_in_line(char *line) {
     int count = 0;
@@ -233,6 +220,80 @@ int count_whitespace(FILE *file) {
 }
 
 
-int is_safe_state(int customer_number, int request[]){
 
+void get_numbers(FILE *file, int count, int numbers[][count]) {
+    char line[1024];
+
+    // Lê linha por linha do arquivo
+    for (int i = 0; i < count; i++) {
+        if (fgets(line, 1024, file) == NULL) {
+            fprintf(stderr, "Error reading numbers from file.\n");
+            exit(EXIT_FAILURE);
+        }
+
+        char *token = strtok(line, " ");
+        int count_numbers = 0;
+
+        // Lê os números separados por espaços
+        while (token != NULL && count_numbers < count) {
+            numbers[i][count_numbers] = atoi(token);
+            count_numbers++;
+
+            token = strtok(NULL, " ");  // Próximos números
+        }
+    }
+
+    // Impressão de debug
+    for (int i = 0; i < count; i++) {
+        for (int j = 0; j < count; j++) {
+            printf("%d ", numbers[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+
+
+
+int is_safe_state(int customer, int request[], int count_resources, int available[], int customer_resources[][count_resources]) {
+    int work[count_resources];
+    int finish[customer];
+
+    // Inicialização dos arrays
+    for (int i = 0; i < count_resources; i++) {
+        work[i] = available[i];
+    }
+
+    for (int i = 0; i < customer; i++) {
+        finish[i] = 0;
+    }
+
+    // Simulação da alocação
+    for (int i = 0; i < customer; i++) {
+        if (finish[i] == 0) {
+            int j;
+            for (j = 0; j < count_resources; j++) {
+                if (customer_resources[i][j] > work[j]) { // Correção 1
+                    break;
+                }
+            }
+
+            if (j == count_resources) {
+                for (int k = 0; k < count_resources; k++) {
+                    work[k] -= request[k]; // Correção 2
+                }
+                finish[i] = 1;
+                i = -1;  // Reinicia a verificação desde o início
+            }
+        }
+    }
+
+    // Verifica se todos os processos foram concluídos
+    for (int i = 0; i < customer; i++) {
+        if (finish[i] == 0) {
+            return 0;  // Não seguro (unsafe) // Correção 3
+        }
+    }
+
+    return 1;  // Seguro (safe)
 }
